4b7488dad1f09184001146a14fedf4657a69594a
Cande Victorica <cvictorica@mail.utdt.edu>
2024-12-03 20:13:37 -0300

    Seven

--- src/App.svelte
+++ src/App.svelte
@@ -137,8 +137,7 @@
   <img src="/images/final vel fuerza resi.png" alt="Habilidades"
   style="width: 40%; display: block; margin-left: auto;margin-right: auto; margin-top: 3%; margin-bottom: 2%;">
   
-  <iframe title="" aria-label="Table" id="datawrapper-chart-zxWxj" src="https://datawrapper.dwcdn.net/zxWxj/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="685" data-external="1"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(a){if(void 0!==a.data["datawrapper-height"]){var e=document.querySelectorAll("iframe");for(var t in a.data["datawrapper-height"])for(var r=0;r<e.length;r++)if(e[r].contentWindow===a.source){var i=a.data["datawrapper-height"][t]+"px";e[r].style.height=i}}}))}();
-  </script>
+  <iframe title="" aria-label="Table" id="datawrapper-chart-zxWxj" src="https://datawrapper.dwcdn.net/zxWxj/1/" scrolling="no" frameborder="0" style="border: none;" width="658" height="685" data-external="1"></iframe>
 
   <!-- Slider para controlar la deformación 
   <div class="container">
