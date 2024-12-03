97747475932a228c3fae6bcf9815e86f79e39933
Cande Victorica <cvictorica@mail.utdt.edu>
2024-12-03 20:08:40 -0300

    Sixth

--- src/App.svelte
+++ src/App.svelte
@@ -136,23 +136,9 @@
   </p>
   <img src="/images/final vel fuerza resi.png" alt="Habilidades"
   style="width: 40%; display: block; margin-left: auto;margin-right: auto; margin-top: 3%; margin-bottom: 2%;">
-  <div
-    style="min-height:685px; display:block; margin-left: auto; margin-right:auto; margin-top: 5%;"
-    id="datawrapper-vis-zxWxj"
-  >
-    <script
-      type="text/javascript"
-      defer
-      src="https://datawrapper.dwcdn.net/zxWxj/embed.js"
-      charset="utf-8"
-      data-target="#datawrapper-vis-zxWxj"
-    ></script><noscript
-      ><img
-        src="https://datawrapper.dwcdn.net/zxWxj/full.png"
-        alt="Heatmap"
-      /></noscript
-    >
-  </div>
+  
+  <iframe title="" aria-label="Table" id="datawrapper-chart-zxWxj" src="https://datawrapper.dwcdn.net/zxWxj/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="685" data-external="1"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(a){if(void 0!==a.data["datawrapper-height"]){var e=document.querySelectorAll("iframe");for(var t in a.data["datawrapper-height"])for(var r=0;r<e.length;r++)if(e[r].contentWindow===a.source){var i=a.data["datawrapper-height"][t]+"px";e[r].style.height=i}}}))}();
+  </script>
 
   <!-- Slider para controlar la deformación 
   <div class="container">
